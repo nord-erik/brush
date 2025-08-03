6db5322302dc02eef17c84e4a9246b7a070b6a73
Erik Nord <erik_nord@outlook.com>
2025-08-03 18:12:30 +0200

    feat: introduce a working build.sh

--- bru.sh
+++ bru.sh
@@ -1,23 +1,185 @@
 #!/bin/env bash
 BRUSH_APP_NAME=$1
 BRUSH_DEFAULT_APP_NAME="bru.sh"
-if [ -z "$BRUSH_APP_NAME" ]; then
-  BRUSH_APP_NAME="$BRUSH_DEFAULT_APP_NAME"
+if [ -z "$BRUSH_APP_NAME" ];then
+BRUSH_APP_NAME="$BRUSH_DEFAULT_APP_NAME"
 fi
 export BRUSH_APP_NAME
 export BRUSH_DEFAULT_APP_NAME
-BRUSH_ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-BRUSH_ROOT="$BRUSH_ROOT/src"
-SWEEPS_ROOT="$BRUSH_ROOT/sweeps"
-source "$BRUSH_ROOT/consts.sh"
-source "$BRUSH_ROOT/logger.sh"
-source "$SWEEPS_ROOT/command.sh"
-source "$SWEEPS_ROOT/env.sh"
-source "$SWEEPS_ROOT/nok.sh"
-source "$SWEEPS_ROOT/ok.sh"
-source "$SWEEPS_ROOT/sudo.sh"
-source "$SWEEPS_ROOT/git/is_clean.sh"
-source "$SWEEPS_ROOT/git/is_init.sh"
-source "$SWEEPS_ROOT/git/is_on.sh"
-unset BRUSH_ROOT
-unset SWEEPS_ROOT
+BRUSH_BLACK=$'\033[30m'
+export BRUSH_BLACK
+BRUSH_RED=$'\033[31m'
+export BRUSH_RED
+BRUSH_GREEN=$'\033[32m'
+export BRUSH_GREEN
+BRUSH_YELLOW=$'\033[33m'
+export BRUSH_YELLOW
+BRUSH_BLUE=$'\033[34m'
+export BRUSH_BLUE
+BRUSH_MAGENTA=$'\033[35m'
+export BRUSH_MAGENTA
+BRUSH_CYAN=$'\033[36m'
+export BRUSH_CYAN
+BRUSH_WHITE=$'\033[37m'
+export BRUSH_WHITE
+BRUSH_LIGHT_BLACK=$'\033[90m'
+export BRUSH_LIGHT_BLACK
+BRUSH_LIGHT_RED=$'\033[91m'
+export BRUSH_LIGHT_RED
+BRUSH_LIGHT_GREEN=$'\033[92m'
+export BRUSH_LIGHT_GREEN
+BRUSH_LIGHT_YELLOW=$'\033[93m'
+export BRUSH_LIGHT_YELLOW
+BRUSH_LIGHT_BLUE=$'\033[94m'
+export BRUSH_LIGHT_BLUE
+BRUSH_LIGHT_MAGENTA=$'\033[95m'
+export BRUSH_LIGHT_MAGENTA
+BRUSH_LIGHT_CYAN=$'\033[96m'
+export BRUSH_LIGHT_CYAN
+BRUSH_LIGHT_WHITE=$'\033[97m'
+export BRUSH_LIGHT_WHITE
+BRUSH_CLEAR=$'\033[0m'
+export BRUSH_CLEAR
+_brush_system_logger(){
+local prio=$1
+local suffix=$2
+local msg=$3
+logger -t "$BRUSH_APP_NAME" -p "user.$prio" "[$$] $suffix $msg"
+}
+_brush_terminal_print(){
+local colour=$1
+local suffix=$2
+local msg=$3
+local to_stderr=$4
+msg=$(printf "%b %s" "$colour$suffix$BRUSH_CLEAR" "$msg")
+if test -z "$to_stderr";then
+printf "%s\n" "$msg"
+else
+printf "%s\n" "$msg" >&2
+fi
+}
+brush_error(){
+local msg=$1
+_brush_system_logger "err" "(ERROR)" "$msg"
+_brush_terminal_print "$BRUSH_RED" "error:" "$msg" to_sderr_please
+}
+brush_warning(){
+local msg=$1
+_brush_system_logger "warn" "(WARNING)" "$msg"
+_brush_terminal_print "$BRUSH_YELLOW" "warning:" "$msg"
+}
+brush_notice(){
+local msg=$1
+_brush_system_logger "notice" "(NOTICE)" "$msg"
+_brush_terminal_print "$BRUSH_BLUE" "notice:" "$msg"
+}
+brush_log(){
+local msg=$1
+_brush_system_logger "info" "(INFO)" "$msg"
+_brush_terminal_print "$BRUSH_WHITE" "info:" "$msg"
+}
+sweep_command(){
+for cmd in "$@";do
+command -v "$cmd" >/dev/null 2>&1
+sweep_ok $? "required command '$cmd' not found"
+done
+}
+sweep_env(){
+local env_var_name=$1
+test ${!env_var_name+1}
+sweep_ok $? "required env variable '$env_var_name' is not set!"
+}
+sweep_nok(){
+local code=$1
+local msg=$2
+local custom_error_code=$3
+if [ -z "$custom_error_code" ];then
+custom_error_code=1
+fi
+if [ $code -ne 1 ];then
+if [ -n "$msg" ];then
+brush_error "$msg"
+fi
+exit $custom_error_code
+fi
+}
+sweep_ok(){
+local code=$1
+local msg=$2
+local custom_error_code=$3
+if [ -z "$custom_error_code" ];then
+custom_error_code=1
+fi
+if [ $code -ne 0 ];then
+if [ -n "$msg" ];then
+brush_error "$msg"
+fi
+exit $custom_error_code
+fi
+}
+sweep_sudo(){
+local should_be=$1
+local is_root final_app_name
+if [ "$BRUSH_APP_NAME" = "$BRUSH_DEFAULT_APP_NAME" ];then
+final_app_name="this script"
+else
+final_app_name="'$BRUSH_APP_NAME'"
+fi
+if [ -z "$should_be" ];then
+should_be=true
+fi
+if [ "$should_be" = "true" ]||[ "$should_be" = "false" ];then
+true
+else
+brush_error "sweep_sudo optional 1st argument must be omitted, true or false"
+exit 1
+fi
+if [ "$(id -u)" -eq 0 ];then
+is_root="true"
+else
+is_root="false"
+fi
+case $is_root in
+true)if
+[ "$should_be" = "true" ]
+then
+return 0
+fi
+brush_error "you must be user in order to run $final_app_name, please re-run without root"
+;;
+false)if
+[ "$should_be" != "true" ]
+then
+return 0
+fi
+brush_error "you must be root in order to run $final_app_name, please re-run with sudo"
+;;
+*)brush_error "unexpected case of quantum root"
+esac
+exit 1
+}
+sweep_git_is_clean(){
+local git_status current_dir
+current_dir="$(pwd)"
+sweep_git_is_init
+git_status="$(git status --porcelain)"&&test -z "$git_status"
+sweep_ok $? "expected git repo to be clean: $current_dir"
+}
+sweep_git_is_init(){
+local is_init current_dir
+current_dir="$(pwd)"
+is_init="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+test "$is_init" == true
+sweep_ok $? "expected this to be a git repo: $current_dir"
+}
+sweep_git_is_on(){
+local expected_branch=$1
+local current_branch current_dir
+test -z "$current_branch"
+sweep_ok $? "you must pass an expected branch name to 'sweep_git_is_on'"
+current_dir="$(pwd)"
+sweep_git_is_init
+current_branch="$(git rev-parse --abbrev-ref HEAD)"
+test "$current_branch" == "$expected_branch"
+sweep_ok $? "expected branch '$expected_branch' but was '$current_branch' in git: $current_dir"
+}
--- build.sh
+++ build.sh
@@ -2,15 +2,64 @@
 # shellcheck disable=SC2016
 
 # builds brush and outputs 'bru.sh'
-
 REPO_ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd "$REPO_ROOT" || exit 112
 
-rm -rf bru.sh
-touch bru.sh
-chmod 644 bru.sh
+# see... brush is very handy now I need to make a small error func
+build_error() {
+  local message=$1
 
-# 1) grep all non-comments and non-empty lines, but include shebang
-# 2) correct root variables by injecting "src"
-grep -v -E "^#[^\!]|^#$|^$" src/brush.sh |
-  sed '/BRUSH_ROOT=/a BRUSH_ROOT="$BRUSH_ROOT/src"' > bru.sh
+  printf "%b%s%b %s\n" $'\033[31m' "error:" $'\033[0m' "$message"
+}
+
+build() {
+  local repo_root=$1
+  local build_brush_root build_sweeps_root build_sources source_file source_file_relative_path
+  build_brush_root=$repo_root/src
+  build_sweeps_root=$repo_root/src/sweeps
+
+  rm -rf bru.sh
+  touch bru.sh
+  chmod 644 bru.sh
+
+  # populate the file
+  grep -v -E "^#[^\!]|^#$|^$" src/brush.sh |                   # grep non-comments, non-empty lines. include shebang
+    sed '/BRUSH_ROOT=/a BRUSH_ROOT="$BRUSH_ROOT/src"' > bru.sh # correct root variables by injecting "src"
+
+  # expand sources to be static
+  # shellcheck disable=SC2207
+  build_sources=($(grep "source" bru.sh | awk '{print $2}'))
+
+  for source_file in "${build_sources[@]}"; do
+    source_file_relative_path=""
+
+    if [[ "$source_file" == *"BRUSH_ROOT"* ]]; then
+      source_file_relative_path="${source_file:13:-1}"
+      source_file_relative_path="$build_brush_root/$source_file_relative_path"
+    fi
+
+    if [[ "$source_file" == *"SWEEPS_ROOT"* ]]; then
+      source_file_relative_path="${source_file:14:-1}"
+      source_file_relative_path="$build_sweeps_root/$source_file_relative_path"
+    fi
+
+    if [ -n "$source_file_relative_path" ]; then
+      grep -v -E "^\s*#|^$" "$source_file_relative_path" >> bru.sh
+    else
+      build_error "unknown source file $source_file quitting..."
+      exit 1
+    fi
+  done
+
+  if ! sed -i '/^\(source\|unset\|BRUSH_ROOT\|SWEEPS_ROOT\)/d' bru.sh; then
+    return 1
+  fi
+
+  shfmt --write --minify bru.sh
+}
+
+if build "$REPO_ROOT"; then
+  printf "%b%s%b %s\n" $'\033[92m' "success:" $'\033[0m' "build all OK"
+else
+  build_error "something went wrong with the build, please inspect bru.sh"
+fi
